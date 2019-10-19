ff116bad09358f17478eb54862fdf0b1f0efa5cf
chris <chris@DESKTOP-5FDPKN5.fritz.box>
2019-10-19 09:04:03 +0200

    git nervt

--- src/org/myoggradio/stb/SpielerStornierenDialog.java
+++ src/org/myoggradio/stb/SpielerStornierenDialog.java
@@ -2,9 +2,6 @@ package org.myoggradio.stb;
 import java.util.ArrayList;
 public interface SpielerStornierenDialog 
 {
-	/*
-	 * Dilog um Spieler aus laufendem Turnier zu entfernen
-	 */
 	public void setSpieler(ArrayList<Spieler> spieler);
 	public void anzeigen();
 }
