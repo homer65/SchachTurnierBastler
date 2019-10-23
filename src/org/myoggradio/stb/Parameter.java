4efad25ab01c348522ad90dbb793d9989bb3ba2b
Christian Ehm <christian@myoggradio.org>
2019-10-23 18:41:14 +0200

    Bugfix Bugfix

--- src/org/myoggradio/stb/Parameter.java
+++ src/org/myoggradio/stb/Parameter.java
@@ -3,17 +3,17 @@ import java.awt.Dimension;
 import java.util.ArrayList;
 public class Parameter 
 {
-	public static String version = "0.17 - 22.10.2019";
+	public static String version = "0.18 - 23.10.2019";
 	public static int anzahlRunden = 7;
 	public static ArrayList<Spieler> spieler = new ArrayList<Spieler>();
 	public static Turnier turnier = Factory.getTurnier();
 	public static ArrayList<Auswertung> auswertungen = new ArrayList<Auswertung>();
 	public static int maxiter = 1000000;
 	public static int itermsg = 100000;
-	public static int malusGleichePartie = 10000;
-	public static int malusMehrAlsEinmalFreilos = 10000;
-	public static int malusFarbdifferenz3 = 9000;
-	public static int malus3malGleicheFarbe = 9500;
+	public static int malusGleichePartie = 1000000;
+	public static int malusMehrAlsEinmalFreilos = 900000;
+	public static int malusFarbdifferenz3 = 900000;
+	public static int malus3malGleicheFarbe = 900000;
 	public static int malusFarbdifferenz2 = 1;
 	public static int malusGleichGut = 25;
 	public static int scrwidth = 800;
