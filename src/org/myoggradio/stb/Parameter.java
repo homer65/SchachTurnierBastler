d1cfd2514ae91accb9568082f7f2ecdbe219d745
homer65 <christian@edv-ehm.de>
2019-10-25 10:46:55 +0200

    Update Parameter.java

--- src/org/myoggradio/stb/Parameter.java
+++ src/org/myoggradio/stb/Parameter.java
@@ -2,7 +2,7 @@ package org.myoggradio.stb;
 import java.util.ArrayList;
 public class Parameter 
 {
-	public static String version = "0.20 - 24.10.2019";
+	public static String version = "0.21 - 25.10.2019";
 	public static int anzahlRunden = 7;
 	public static ArrayList<Spieler> spieler = new ArrayList<Spieler>();
 	public static Turnier turnier = Factory.getTurnier();
@@ -10,7 +10,7 @@ public class Parameter
 	public static int maxiter = 1000000;
 	public static int itermsg = 100000;
 	public static int malusGleichePartie = 1000000;
-	public static int malusMehrAlsEinmalFreilos = 900000;
+	public static int malusMehrAlsEinmalFreilos = 950000;
 	public static int malusFarbdifferenz3 = 900000;
 	public static int malus3malGleicheFarbe = 900000;
 	public static int malusFarbdifferenz2 = 1;
