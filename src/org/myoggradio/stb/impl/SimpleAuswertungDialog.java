09bc162d4a90b34c0e92d92be92c0452b9ab0581
homer65 <christian@edv-ehm.de>
2019-10-25 13:15:41 +0200

    Update SimpleAuswertungDialog.java

--- src/org/myoggradio/stb/impl/SimpleAuswertungDialog.java
+++ src/org/myoggradio/stb/impl/SimpleAuswertungDialog.java
@@ -29,7 +29,14 @@ public class SimpleAuswertungDialog extends JDialog implements AuswertungDialog
 	public void anzeigen()
 	{
 		JPanel cpan = new JPanel();
-		cpan.setLayout(new GridLayout(ausw.size(),5));
+		cpan.setLayout(new GridLayout(ausw.size()+1,7));
+		cpan.add(new JLabel("Tisch Nummer"));
+		cpan.add(new JLabel("Spieler"));
+		cpan.add(new JLabel("Punkte"));
+		cpan.add(new JLabel("Buchholz"));
+		cpan.add(new JLabel("DWZ"));
+		cpan.add(new JLabel("Anzahl Weiss"));
+		cpan.add(new JLabel("Anzahl Schwarz"));
 		for (int i=0;i<ausw.size();i++)
 		{
 			Auswertung auswertung = ausw.get(i);
@@ -37,16 +44,22 @@ public class SimpleAuswertungDialog extends JDialog implements AuswertungDialog
 			double punkte = auswertung.getPunkte();
 			double buchholz = auswertung.getBuchholz();
 			int dwz = spieler.getDWZ();
+			int anzahlWeiss = auswertung.getAnzahlWeiss();
+			int anzahlSchwarz = auswertung.getAnzahlSchwarz();
 			JLabel lab1 = new JLabel("   " + (i+1));
 			JLabel labs = new JLabel(spieler.getVorname() + " " + spieler.getName());
 			JLabel labp = new JLabel("   " + punkte);
 			JLabel labb = new JLabel("   " + buchholz);
 			JLabel labd = new JLabel("   " + dwz);
+			JLabel labaw = new JLabel("  " + anzahlWeiss);
+			JLabel labas = new JLabel("  " + anzahlSchwarz);
 			cpan.add(lab1);
 			cpan.add(labs);
 			cpan.add(labp);
 			cpan.add(labb);
 			cpan.add(labd);
+			cpan.add(labaw);
+			cpan.add(labas);
 		}
 		JScrollPane scrpan=new JScrollPane(cpan);
 		scrpan.setPreferredSize(new Dimension(Parameter.scrwidth,Parameter.scrheight));
