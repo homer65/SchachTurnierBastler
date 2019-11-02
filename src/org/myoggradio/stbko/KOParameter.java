4e7a0d58fe6c5cdba118ec29b2f58ff389169ab0
Christian Ehm <christian@myoggradio.org>
2019-11-02 17:58:41 +0100

    Git nervt

---
+++ src/org/myoggradio/stbko/KOParameter.java
@@ -0,0 +1,8 @@
+package org.myoggradio.stbko;
+import java.util.ArrayList;
+import org.myoggradio.stb.Spieler;
+public class KOParameter 
+{
+	public static ArrayList<Spieler> spieler = new ArrayList<Spieler>();
+	public static ArrayList<Spieler> gesetzteSpieler = new ArrayList<Spieler>();
+}
