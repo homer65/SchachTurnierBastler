148c38846d8df441d9cc6a7003f6df76066d58c0
Christian Ehm <christian@myoggradio.org>
2019-11-04 08:58:20 +0100

    git nervt

--- src/org/myoggradio/stbko/impl/SimpleKOTurnierMenu2.java
+++ src/org/myoggradio/stbko/impl/SimpleKOTurnierMenu2.java
@@ -5,8 +5,6 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.TableColumn;
 import org.myoggradio.stb.*;
-import org.myoggradio.stb.Shutdown;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
