eef13819146ae94f188a336b9065fb26ff828287
Christian <christian@edv-ehm.de>
2022-07-23 17:01:01 +0200

    Neue Version

--- src/org/myoggradio/stb/Parameter.java
+++ src/org/myoggradio/stb/Parameter.java
@@ -2,7 +2,7 @@ package org.myoggradio.stb;
 import java.util.ArrayList;
 public class Parameter 
 {
-	public static String version = "1.34 - 23.07.2022";
+	public static String version = "1.40 - 23.07.2022";
 	public static int anzahlRunden = 7;
 	public static ArrayList<Spieler> spieler = new ArrayList<Spieler>();
 	public static Turnier turnier = Factory.getTurnier();
